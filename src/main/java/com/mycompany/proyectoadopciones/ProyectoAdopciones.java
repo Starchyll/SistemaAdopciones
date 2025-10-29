7197c528f8bd5a4c50ef46d4534821e8339bc13d
AncorMx <angelcortez05@hotmail.com>
2025-10-29 09:03:34 -0700

    Creacion proyecto Adopciones

---
+++ src/main/java/com/mycompany/proyectoadopciones/ProyectoAdopciones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoadopciones;
+
+/**
+ *
+ * @author angel
+ */
+public class ProyectoAdopciones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
